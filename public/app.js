9dd96fd6efca4a6b2d9468c251b1846ba9a37d81
Paul <lazar_paul_2006@yahoo.com>
2023-07-23 19:58:20 +0100

    Added function to encode auth data and updated fetchData function. Data is visibil in the console.

--- public/app.js
+++ public/app.js
@@ -1,15 +1,26 @@
+// Function that encodes authentication data
+function encode(str) {
+  return btoa(str);
+}
+
 // Function that fetches EPC data
 function fetchData(postCode) {
-  const apiKey = '3c323f56a61174e6b2da8aef5502e693aeef2b1e'
-  const apiUrl = `https://epc.opendatacommunities.org/api/v1/domestic/search?postcode=${postCode}`
+  const email = "lazar_paul_2006@yahoo.com";
+  const apiKey = '3c323f56a61174e6b2da8aef5502e693aeef2b1e';
+  const encodedAuthData = encode(`${email}:${apiKey}`);
+  const apiUrl = `https://epc.opendatacommunities.org/api/v1/domestic/search?postcode=${postCode}`;
+  const headers = {
+    "Accept": "text/csv",
+    "Authorization": `Basic ${encodedAuthData}`
+  }
 
-  return fetch(apiUrl)
-    .then((response) => response.json())
-    .then((jsonData) => {
-      console.log(jsonData)
+  return fetch(apiUrl, {headers})
+    .then((response) => response.text())
+    .then((csvData) => {
+      console.log(csvData)
     })
     .catch((error) => {
-        console.error("Cannot fetch data",error)
+        console.error("Cannot fetch data", error)
     }) 
 }
 
