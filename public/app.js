4f743bcb89ffafdfe3ac8cd4675f3cee0fc9a2bc
Paul <lazar_paul_2006@yahoo.com>
2023-07-30 22:34:06 +0100

    Moved email and apiKey to config file.

--- public/app.js
+++ public/app.js
@@ -5,8 +5,6 @@ function encode(str) {
 
 // Function that fetches EPC data
 function fetchData(postCode) {
-  const email = "lazar_paul_2006@yahoo.com";
-  const apiKey = '3c323f56a61174e6b2da8aef5502e693aeef2b1e';
   const encodedAuthData = encode(`${email}:${apiKey}`);
   const apiUrl = `https://epc.opendatacommunities.org/api/v1/domestic/search?postcode=${postCode}`;
   const headers = {
---
+++ public/config.js
@@ -0,0 +1,3 @@
+const apiKey = '3c323f56a61174e6b2da8aef5502e693aeef2b1e';
+const email = 'lazar_paul_2006@yahoo.com';
+
